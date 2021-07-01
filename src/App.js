aa22e5244a2f96882983b606d9ce6aaa11de256c
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-01 14:28:41 +0200

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout/MainLayout';
+import Dashboard from './components/views/Dashboard/Dashboard';
+import Kitchen from './components/views/Kitchen/Kitchen';
+import Login from './components/views/Login/Login';
+import Waiter from './components/views/Waiter/Waiter';
+import Tables from './components/views/Tables/Tables';
+import Booking from './components/views/Booking/Booking';
+import Event from './components/views/Event/Event';
+import Order from './components/views/Order/Order';
+
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <MainLayout>
-        <header className="App-header">
-          <p>
-         Learn reacth<code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL + '/'} component={Dashboard} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={Event} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={Order} />
+        </Switch>
       </MainLayout>
-    </div>
+    </BrowserRouter>
   );
 }
 
